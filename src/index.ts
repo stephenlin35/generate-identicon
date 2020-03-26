fa4ea9b0aa8c7526dce47514483e870e978f330e
Stephen Lin <stephenlin@Stephens-MBP.fios-router.home>
2020-03-26 03:01:27 -0400

    rewrite randomstr func into subdir

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import generateString from "./util/randomize";
+
+console.log(generateString());
+console.log(generateString());
+console.log(generateString());
---
+++ src/util/randomize.ts
@@ -0,0 +1,17 @@
+const generateRandomString = () => {
+  const char_selection = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789abcdefghijklmnopqrstuvwxyz";
+  const output_length = _randomize(40);
+  let output = "";
+
+  for (let i = 0; i < output_length; i++) {
+    output += char_selection.charAt(_randomize(char_selection.length));
+  }
+
+  return output;
+};
+
+const _randomize = (length: number) => {
+  return Math.floor(Math.random() * length);
+};
+
+export default generateRandomString;
